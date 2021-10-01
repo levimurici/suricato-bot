e98a5cde414a4797f7d550039d3bc6d30c5b92e7
levimurici <levi.murici@gmail.com>
2021-10-01 16:56:19 -0300

    Dividindo bots

---
+++ suricato-watcher.py
@@ -0,0 +1,69 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import requests
+from telegram.ext.callbackcontext import CallbackContext
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+bot_token = '2033615689:AAHOKrylOujHa9fcPJGLn25Yhd78-luj5PQ'
+
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+status_out = ''
+warning_out = ''
+status_alarms = {"janela1": '', 'janela2': '', 'janela3': ''}
+
+def start(update, context):
+    context.bot.send_message\
+        (chat_id=update.effective_chat.id,\
+            text="Fala aí, beleza?!\n" \
+            "Sou o bot responsável por controlar os alarmes! :)")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def status(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=status_out)
+    
+def warning(context: CallbackContext):
+    job = context.job
+    context.bot.send_message(job.context, text=warning_out)
+
+def text_warning(window):
+    texto = 'Se liga, a janela {window} tá aberta!\n'\
+            'Dá uma olhada pra ver quem tá lá'
+    return texto
+
+def text_status(status1, status2, status3):
+    texto = 'A janela1 está {status1}!\n'\
+            'A janela2 está {status2}!\n'\
+            'A janela3 está {status3}!\n'
+    return texto
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('status', status))
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+
+if __name__ == '__main__':
+    while True:
+        response = requests.get("http://192.168.101.28:3000/alarm/pull")
+        status = response.json()
+        def warning_check(status):
+            for spot in status['sensors']:
+                if spot['control'] == 'true':
+                    print('O dispositivo {} foi desativado'.format(spot['name']))
+                    warning_out = text_warning(spot('name'))
+                    warning()
+                    
+                if spot['name'] == 'device1/Relay':
+                    status_alarms['janela1'] = spot['control']
+                    print("1")
+                elif spot['name'] == 'device2/Relay':
+                    status_alarms['janela2'] = spot['control']
+                    print("2")
+                elif spot['name'] == 'device3/Relay':
+                    status_alarms['janela3'] = spot['control']
+                    print("3")
+            status_out = text_status(status_alarms['janela1'], status_alarms['janela2'], status_alarms['janela3'])
+        print(status)
+        updater.start_polling()
+        # updater.idle()
