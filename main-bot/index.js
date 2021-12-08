82582920f20f619b8b825fa08f353c04f03493fa
Levi Murici <levi.murici@gmail.com>
2021-12-08 13:13:29 -0300

    Rotas security prontas

--- main-bot/index.js
+++ main-bot/index.js
@@ -44,23 +44,23 @@ bot.onText(/\/ajuda/, (msg, match) => {
 })
 
 bot.onText(/\/status_alarmes/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1];
-    let sendData = '';
-    
-    let getData_alarm = require('./routes/alarms/index.js');
-    getData_alarm(function(string_alarm){
-      sendData = string_alarm;
-      console.log(string_alarm)
-      if (sendData === undefined) {
-        bot.sendMessage(chatId, "data_undefined");
-        console.log(`request from ${chatId} and ${sendData}`)
-      }
-      else {
-        bot.sendMessage(chatId, sendData);
-        console.log(`request from ${chatId}`)
-      }  
-    });    
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  let sendData = '';
+  
+  const getData_alarm = require('./routes/alarms/index.js');
+  getData_alarm(function(string_alarm){
+    sendData = string_alarm;
+    console.log(string_alarm)
+    if (sendData === undefined) {
+      bot.sendMessage(chatId, "data_undefined");
+      console.log(`request from ${chatId} and ${sendData}`)
+    }
+    else {
+      bot.sendMessage(chatId, sendData);
+      console.log(`request from ${chatId}`)
+    }  
+  });    
 });
   
 bot.onText(/\/status_jardim/, (msg, match) => {
@@ -68,7 +68,7 @@ bot.onText(/\/status_jardim/, (msg, match) => {
   const resp = match[1];
   let sendData = '';
   
-  let getData_garden = require('./routes/gardener/index.js');
+  const getData_garden = require('./routes/gardener/index.js');
   getData_garden(function(string_garden){
     sendData = string_garden;
     console.log(string_garden)
@@ -87,14 +87,25 @@ bot.onText(/\/seguranca_on/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
 
-  bot.sendMessage(chatId, '🏗️Watchdog trigger em Construção🏗️');
+  const getSecurityMode = require('./routes/security/on')
+  getSecurityMode(function(string_security){
+    console.log(`\nrequest from ${chatId}, apiResponse: ${string_security}`)
+    console.log("Security ON")
+  });
+  bot.sendMessage(chatId, `Modo de segurança ON`);
 });
 
 bot.onText(/\/seguranca_off/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
   
-  bot.sendMessage(chatId, '🏗️Watchdog trigger em Construção🏗️');
+  const getSecurityMode = require('./routes/security/off')
+  getSecurityMode(function(string_security){
+    console.log(`\nrequest from ${chatId}, apiResponse: ${string_security}`)
+    console.log("Security OFF")
+  });
+  bot.sendMessage(chatId, `Modo de segurança OFF`);
+  
 });
 
 
---
+++ main-bot/routes/security/off.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+module.exports = function (callback){
+const  jsonSend = {
+  "status": "false"
+}
+
+axios.post("http://192.168.1.2:3000/watchdog/security-mode/dump", jsonSend, {
+  headers:{
+    'content-type': 'application/json'
+  }
+})
+.then(res => {
+  callback(`${res.status}`)
+})
+.catch(error =>{
+  console.error(error)
+})
+}
