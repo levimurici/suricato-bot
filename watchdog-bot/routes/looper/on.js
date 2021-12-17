e33342a2fe826f71ecbf03ea5cc0a3cab939df22
Levi Murici <levi.murici@gmail.com>
2021-12-17 13:58:39 -0300

    Modificado o AxiosPost

--- watchdog-bot/routes/looper/on.js
+++ watchdog-bot/routes/looper/on.js
@@ -1,11 +1,16 @@
 const axios = require('axios')
+const config = require('config')
+
+const api_url = config.get('api.address')
+const api_port = config.get('api.port')
+const route = "/watchdog/security-loop/dump"
 
 module.exports = function (callback){
 const  jsonSend = {
   "loop": "true"
 }
 
-axios.post("http://192.168.1.10:3000/watchdog/security-loop/dump", jsonSend, {
+axios.post(api_url+":"+api_port+route, jsonSend, {
   headers:{
     'content-type': 'application/json'
   }
