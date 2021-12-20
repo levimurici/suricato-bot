ca9d25d9c630f454dcf164a44a122760156ebdb1
Levi Murici <levi.murici@gmail.com>
2021-12-20 15:09:25 -0300

    Corrigindo portas dos containers - usando dns rede-docker

--- watchdog-bot/routes/looper/off.js
+++ watchdog-bot/routes/looper/off.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 const config = require('config')
 
 /* const api_url = config.get('api.address')
-const api_port = config.get('api.port') */
+const api_port = config.get('api.port')
 const api_url = "api"
-const api_port = "3000"
+const api_port = "3000" */
 const route = "/watchdog/security-loop/dump"
 
 module.exports = function (callback){
@@ -12,7 +12,12 @@ const  jsonSend = {
   "loop": "false"
 }
 
-axios.post(api_url+":"+api_port+route, jsonSend, {
+/* axios.post("https://"+api_url+":"+api_port+route, jsonSend, {
+  headers:{
+    'content-type': 'application/json'
+  } */
+
+axios.post("https://api:3000"+route, jsonSend, {
   headers:{
     'content-type': 'application/json'
   }
